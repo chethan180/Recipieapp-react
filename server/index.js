7bfdfe624a17f8b7c9a94497ac714648ff7f239f
chethan180 <chyten9@gmail.com>
2021-08-24 15:01:33 +0530

    concur

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ app.use("/user", userRouter);
 const CONNECTION_URL = `mongodb+srv://${user}:${pswrd}@cluster0.p6xod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT|| 5000;
 
+
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
